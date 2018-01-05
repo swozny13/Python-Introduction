ca8555b204541d71eed2727462a5bf9bb842e33c
swozny13 <sebastianwozniak91@gmail.com>
2018-01-05 10:40:23 +0100

    add exponentiation and factorial

--- calc.py
+++ calc.py
@@ -33,6 +33,23 @@ def dzielenie():
     print("Wynik działania: {}".format(round(wynik, 2)))
 
 
+def potegowanie():
+    print("POTĘGOWANIE")
+    a = input("Podaj liczbe potęgowaną: ")
+    b = int(input("Podaj, do której potęgi chcesz podnieść liczbe {}: ".format(a)))
+    wynik = float(a) ** int(b)
+    print("Wynik potęgowania dla: {}^{} wynosi: {}".format(a, b, wynik))
+
+
+def silnia(a):
+    if a > 1:
+        wynik = 0
+        wynik = a * silnia(a - 1)
+    else:
+        return 1
+    return wynik
+
+
 def start():
     flaga = True
     while flaga:
@@ -41,7 +58,9 @@ def start():
         print("2. Odejmowanie")
         print("3. Mnożenie")
         print("4. Dzielenie")
-        print("5. Zakończ")
+        print("5. Potęgowanie")
+        print("6. Silnia")
+        print("7. Zakończ program")
         opcjaWyboru = int(input("Wybierz działanie: "))
 
         if opcjaWyboru == 1:
@@ -53,9 +72,17 @@ def start():
         elif opcjaWyboru == 4:
             dzielenie()
         elif opcjaWyboru == 5:
+            potegowanie()
+        elif opcjaWyboru == 6:
+            print("SILNIA")
+            a = int(input("Podaj liczbe do obliczenia silni: "))
+            wynik = silnia(a)
+            print("Wynik silni z liczby {} wynosi: {}".format(a, wynik))
+        elif opcjaWyboru == 7:
             print("ZAKOŃCZONO")
             break
         else:
+            print("Nie ma takiej opcji. Zakończono program")
             flaga = False
             break
 
