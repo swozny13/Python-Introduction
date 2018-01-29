28bf9e67b5b0a2eb804faab815a7fc0537e2850d
swozny13 <sebastianwozniak91@gmail.com>
2018-01-29 22:43:39 +0100

    pyladies 1 meet -> very simple functions

--- pyladies/pl1.py
+++ pyladies/pl1.py
@@ -50,45 +50,68 @@ country = {"Poland": "Witaj", "England": "Hello", "Germany": "Hallo", "Italy": "
 #
 
 def menu():
-    print("1. Zapytaj o kraj")
-    print("2. Zapytaj o imię")
-    print("3. Sprawdź czy kraj jest w mojej strukturze")
-    print("4. Wyświetl coś....")
-    print("5. Koniec programu")
+    print()
 
-    a = input("Wybierz opcje")
+    flaga = True
+    while flaga:
+        print("1. Zapytaj o kraj")
+        print("2. Zapytaj o imię")
+        print("3. Sprawdź czy kraj jest w mojej strukturze")
+        print("4. Wyświetl coś....")
+        print("5. Koniec programu")
 
-    if a == 1:
-        get_country()
-    if a == 2:
-        get_name()
-    if a == 3:
-        found_country()
-    if a == 4:
-        print_sth()
-    if a == 5:
-        print()
+        a = int(input("Wybierz opcje: "))
 
-
-# flag = false
+        if a == 1:
+            get_country()
+        elif a == 2:
+            get_name()
+        elif a == 3:
+            found_country()
+        elif a == 4:
+            print_sth()
+        elif a == 5:
+            print("ZAKOŃCZONO")
+            flaga = False
+        else:
+            print("Nie ma takiej opcji")
 
 
 def get_country():
+    print()
+    country = input("Hej! Z jakiego kraju jesteś?: ")
+    print("Super! :-) Pozdrowienia dla {}".format(country))
     print()
 
 
 def get_name():
     print()
+    name = input("Napisz mi jak masz na imię: ")
+    print("Cześć, {}!".format(name))
+    print()
 
 
 def found_country():
-    x = input("Podaj nazwe kraju...")
+    print()
+    x = input("Podaj nazwe kraju (po angielsku): ")
 
     if x in country.keys():
-        print("Kraj istnieje")
+        print("{} istnieje w moim słowniku".format(x))
     else:
-        print("Kraj nie istieje")
+        print("{} nie istieje w moim słowniku".format(x))
+    print()
 
 
 def print_sth():
     print()
+    file = open("cos.txt", "r", encoding='utf-8')
+    print(file.readlines())
+    file.close()
+    print()
+
+
+def exit_program(flaga):
+    flaga = False
+
+
+menu()
