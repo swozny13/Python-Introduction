6cdc11e2b5c47fdd0a16766c8ce71865d73c7d4a
swozny13 <sebastianwozniak91@gmail.com>
2018-02-16 15:57:48 +0100

    pyladies 1 meet -> very simple functions

--- gui/radio.py
+++ gui/radio.py
@@ -13,13 +13,13 @@ class RadioBox(Frame):
 
         self.favorite = StringVar()
         self.favorite.set(None)
-        Radiobutton(self, text="schabowy", variable=self.favorite, value="schabowy.", command=self.update_text).grid(
+        Radiobutton(self, text="schabowy", variable=self.favorite, value="schabowy", command=self.update_text).grid(
             row=1, column=0, sticky=W)
-        Radiobutton(self, text="stek", variable=self.favorite, value="stek.", command=self.update_text).grid(
+        Radiobutton(self, text="stek", variable=self.favorite, value="stek", command=self.update_text).grid(
             row=2, column=0, sticky=W)
-        Radiobutton(self, text="sałatka", variable=self.favorite, value="sałatka.", command=self.update_text).grid(
+        Radiobutton(self, text="sałatka", variable=self.favorite, value="sałatka", command=self.update_text).grid(
             row=3, column=0, sticky=W)
-        Radiobutton(self, text="rosół", variable=self.favorite, value="rosół.", command=self.update_text).grid(
+        Radiobutton(self, text="rosół", variable=self.favorite, value="rosół", command=self.update_text).grid(
             row=4, column=0, sticky=W)
 
         self.result_text = Text(self, width=40, height=5, wrap=WORD)
@@ -28,7 +28,7 @@ class RadioBox(Frame):
         self.button = Button(self, text="Prześlij").grid(row=6, column=2, sticky=W)
 
     def update_text(self):
-        message = "Twoją ulubioną potrawą jest " + str(self.favorite.get())
+        message = "Twoją ulubioną potrawą jest " + str(self.favorite.get()) + "."
 
         self.result_text.delete(0.0, END)
         self.result_text.insert(0.0, message)
